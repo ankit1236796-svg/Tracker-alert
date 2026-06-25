4139cfeaf3546eda266c473e49afcc345b5c1ecf
ankit1236796-svg <ankit1236796@gmail.com>
2026-06-26 05:23:03 +0530

    Update states.py

--- states.py
+++ states.py
@@ -1,10 +1,5 @@
 from aiogram.fsm.state import State, StatesGroup
 
-
-class AddProductState(StatesGroup):
+class AddProduct(StatesGroup):
     waiting_for_name = State()
     waiting_for_link = State()
-
-
-class RemoveProductState(StatesGroup):
-    waiting_for_product_id = State()
