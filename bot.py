1009d7c9e95e33531a8339cf81ee9fb5f0dc6ce3
ankit1236796-svg <ankit1236796@gmail.com>
2026-06-26 05:12:31 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,16 @@
+from aiogram import Bot, Dispatcher
+from aiogram.enums import ParseMode
+from aiogram.client.default import DefaultBotProperties
 
+from config import BOT_TOKEN
+
+bot = Bot(
+    token=BOT_TOKEN,
+    default=DefaultBotProperties(parse_mode=ParseMode.HTML)
+)
+
+dp = Dispatcher()
+
+
+async def main():
+    print("Tracker Alert Bot Started")
